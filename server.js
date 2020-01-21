a024cd369f750d225bd238ca02b97f7a974cb1ce
Raja Polavarapu <raja.polavarapu@gmail.com>
2020-01-21 13:00:59 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/sudoku-live'));
+app.use(express.static('/dist/sudoku-live'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile('/dist/sudoku-live/index.html'));
+    res.send('hello world');
+//res.sendFile('/dist/sudoku-live/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
